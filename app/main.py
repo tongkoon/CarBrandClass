5da79076a47a38531c1092278b11e500ef9fcd21
tongkoon <64011212078@msu.ac.th>
2023-08-18 01:21:13 +0700

    Dockerfile and requirements

--- app/main.py
+++ app/main.py
@@ -1,19 +1,25 @@
 import base64
 import pickle
-from code import predict_brand
 
 import cv2
 import numpy as np
 import requests
 from fastapi import FastAPI, Request
 
-# from app.code import predict_brand
+# use docker
+from app.code import predict_brand
+
+# from code import predict_brand
 
 app = FastAPI()
-model = pickle.load(open(r'C:\Users\User\Desktop\AI\Assignment1\CarBrandClass\model\model_XGB.pkl','rb'))
-# model = pickle.load(open(f'model/model_XGB.pkl','rb'))
-end_hog = 'http://localhost:8080/api/gethog'
-# end_hog = 'http://172.17.0.2:80/api/gethog'
+
+# model = pickle.load(open(r'C:\Users\User\Desktop\AI\Assignment1\CarBrandClass\model\model_XGB.pkl','rb'))
+# use docker
+model = pickle.load(open(f'model/model_XGB.pkl','rb'))
+
+# end_hog = 'http://localhost:8080/api/gethog'
+# use docker
+end_hog = 'http://172.17.0.2:80/api/gethog'
 
 @app.get("/")
 def root():
